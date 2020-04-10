8729aabe8cf715d9abc3f4ef5cf48450b8eb55a6
Acethu <Mhondra@t-online.de>
2020-04-10 16:57:54 +0200

    basic color replacement function

---
+++ replace_function.py
@@ -0,0 +1,26 @@
+from PIL import Image
+
+def replace(image, old_color, new_color):
+    """
+    Replaces one color of an image with another one.
+
+    image : str
+        a string refering to the images file path
+
+    old_color : tuple
+        the target color to replace
+
+    new_color : tuple
+        the new color to set the pixels to
+    """
+    # open image
+    rgb_image = Image.open(image).convert("RGB")
+    # run through all pixels
+    for x in range(rgb_image.width):
+        for y in range(rgb_image.height):
+            pixel = rgb_image.getpixel((x,y))
+            # replace all pixel colors
+            if pixel == old_color:
+                rgb_image.putpixel((x,y), new_color)
+    # return new image
+    return rgb_image
